108ee587bcc8f0ad874122f5dcd574b3aebcd5b9
CrystalHerda <crystaljoyherda@gmail.com>
2025-08-04 23:24:21 -0700

    Added Bootsrap template and Home controller

---
+++ app/Views/template.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>ITE311</title>
+
+  <!-- Local Bootstrap (from public/) -->
+  <link href="<?= base_url('public/css/bootstrap.min.css'); ?>" rel="stylesheet">
+  <script src="<?= base_url('public/js/bootstrap.bundle.min.js'); ?>"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ITE311</a>
+  </div>
+</nav>
+
+<div class="container mt-4">
+  <h1>Hello from CodeIgniter 4.6.3 + Bootstrap!</h1>
+</div>
+
+</body>
+</html>
