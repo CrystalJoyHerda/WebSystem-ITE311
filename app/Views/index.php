d728de2812568bf85c058fc81d64ea827bee34ec
CrystalHerda <crystaljoyherda@gmail.com>
2025-08-26 20:59:40 -0700

    Added Home controller, routes, and views for basic navigation

---
+++ app/Views/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+     <ul class="nav">
+        <li class="nav-item">
+         <a class="nav-link active" href="/">Homepage</a>
+          </li>
+          <li class="nav-item">
+             <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link home" href="/contact">Contact</a>
+          </li>
+     </ul>
+  </div>
+</nav>
+    <h1>Home</h1>
+</body>
+</html>
